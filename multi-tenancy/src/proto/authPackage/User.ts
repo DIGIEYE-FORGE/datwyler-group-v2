8284b3e85863bcab55d5a55556bf6b7f37c327fb
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2023-05-21 13:53:11 +0100

    add data

--- multi-tenancy/src/proto/authPackage/User.ts
+++ multi-tenancy/src/proto/authPackage/User.ts
@@ -1,14 +1,12 @@
 // Original file: auth.proto
 
-import type { Any as _google_protobuf_Any, Any__Output as _google_protobuf_Any__Output } from '../google/protobuf/Any';
 
 export interface User {
   'id'?: (number);
   'firstName'?: (string);
   'lastName'?: (string);
   'email'?: (string);
-  'password'?: (string);
-  'avatar'?: (_google_protobuf_Any | null);
+  'avatar'?: (string);
 }
 
 export interface User__Output {
@@ -16,6 +14,5 @@ export interface User__Output {
   'firstName'?: (string);
   'lastName'?: (string);
   'email'?: (string);
-  'password'?: (string);
-  'avatar'?: (_google_protobuf_Any__Output);
+  'avatar'?: (string);
 }
