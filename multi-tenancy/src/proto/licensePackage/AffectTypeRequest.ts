84f353defd27c205425c090b44ec006da89914c5
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2023-05-26 01:46:53 +0100

    license

---
+++ multi-tenancy/src/proto/licensePackage/AffectTypeRequest.ts
@@ -0,0 +1,29 @@
+// Original file: tenant-liscence.proto
+
+
+// Original file: tenant-liscence.proto
+
+export const _licensePackage_AffectTypeRequest_Type = {
+  USERS: 0,
+  DATACENTER: 1,
+} as const;
+
+export type _licensePackage_AffectTypeRequest_Type =
+  | 'USERS'
+  | 0
+  | 'DATACENTER'
+  | 1
+
+export type _licensePackage_AffectTypeRequest_Type__Output = typeof _licensePackage_AffectTypeRequest_Type[keyof typeof _licensePackage_AffectTypeRequest_Type]
+
+export interface AffectTypeRequest {
+  'type'?: (_licensePackage_AffectTypeRequest_Type);
+  'tenantId'?: (number);
+  'typeId'?: (number);
+}
+
+export interface AffectTypeRequest__Output {
+  'type'?: (_licensePackage_AffectTypeRequest_Type__Output);
+  'tenantId'?: (number);
+  'typeId'?: (number);
+}
---
+++ multi-tenancy/src/proto/licensePackage/AuthPermissionResponse.ts
@@ -0,0 +1,10 @@
+// Original file: tenant-liscence.proto
+
+
+export interface AuthPermissionResponse {
+  'permission'?: (boolean);
+}
+
+export interface AuthPermissionResponse__Output {
+  'permission'?: (boolean);
+}
